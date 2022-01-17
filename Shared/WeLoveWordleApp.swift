c42df05c2f41df4f2400924ba105874204df130d
Mark Powell <Mark.Powell@computer.org>
2022-01-17 11:50:19 -0800

    Initial Commit

---
+++ Shared/WeLoveWordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeLoveWordleApp.swift
+//  Shared
+//
+//  Created by Mark Powell on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WeLoveWordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
